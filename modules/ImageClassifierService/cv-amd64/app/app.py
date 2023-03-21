cbb98e6198bad5340f25ff8b02aff32b56fc3776
Ubuntu <soumaya.ihihi@sncf.fr>
2023-03-21 15:55:34 +0100

    Adapt to yolov5 inference

--- modules/ImageClassifierService/cv-amd64/app/app.py
+++ modules/ImageClassifierService/cv-amd64/app/app.py
@@ -1,4 +1,5 @@
-
+import argparse
+import torch
 import json
 import os
 import io
@@ -10,9 +11,10 @@ from flask import Flask, request, jsonify
 from PIL import Image
 
 # Imports for prediction
-from predict import initialize, predict_image, predict_url
+# from predict import initialize, predict_image, predict_url
 
 app = Flask(__name__)
+models = {}
 
 # 4MB Max image size limit
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024 
@@ -32,7 +34,9 @@ def index():
 @app.route('/<project>/classify/iterations/<publishedName>/image/nostore', methods=['POST'])
 @app.route('/<project>/detect/iterations/<publishedName>/image', methods=['POST'])
 @app.route('/<project>/detect/iterations/<publishedName>/image/nostore', methods=['POST'])
-def predict_image_handler(project=None, publishedName=None):
+def predict(model='yolov5_custom', project=None, publishedName=None):
+    if request.method != 'POST':
+        return
     try:
         imageData = None
         if ('imageData' in request.files):
@@ -43,36 +47,32 @@ def predict_image_handler(project=None, publishedName=None):
             imageData = io.BytesIO(request.get_data())
 
         img = Image.open(imageData)
-        results = predict_image(img)
-        return jsonify(results)
+        if model in models:
+            results = models[model](img, size=640)  # reduce size=320 for faster inference
+            return results.pandas().xyxy[0].to_json(orient='records')
+        
     except Exception as e:
         print('EXCEPTION:', str(e))
         return 'Error processing image', 500
 
 
-# Like the CustomVision.ai Prediction service /url route handles url's
-# in the body of hte request of the form:
-#     { 'Url': '<http url>'}  
-@app.route('/url', methods=['POST'])
-@app.route('/<project>/url', methods=['POST'])
-@app.route('/<project>/url/nostore', methods=['POST'])
-@app.route('/<project>/classify/iterations/<publishedName>/url', methods=['POST'])
-@app.route('/<project>/classify/iterations/<publishedName>/url/nostore', methods=['POST'])
-@app.route('/<project>/detect/iterations/<publishedName>/url', methods=['POST'])
-@app.route('/<project>/detect/iterations/<publishedName>/url/nostore', methods=['POST'])
-def predict_url_handler(project=None, publishedName=None):
-    try:
-        image_url = json.loads(request.get_data().decode('utf-8'))['url']
-        results = predict_url(image_url)
-        return jsonify(results)
-    except Exception as e:
-        print('EXCEPTION:', str(e))
-        return 'Error processing image'
-
 if __name__ == '__main__':
-    # Load and intialize the model
-    initialize()
+    parser = argparse.ArgumentParser(description='Flask API exposing YOLOv5 model')
+    parser.add_argument('--port', default=80, type=int, help='port number')
+    parser.add_argument('--model', nargs='+', default='yolov5_custom', help='model(s) to run, i.e. --model yolov5n yolov5s')
+    opt = parser.parse_args()
 
-    # Run the server
-    app.run(host='0.0.0.0', port=80)
+    for m in opt.model:
+        if m == 'yolov5_custom':
+            models[m] = torch.hub.load('/yolov5', 'custom', 'best.pt', source='local', force_reload=True,)
+            # torch.hub.load('.', 'custom', source='local', force_reload=True)
+            # models[m].load_state_dict(torch.load('/Downloads/test-yolo-detect/yolov5/inputs/model/best.pt', map_location=torch.device('cpu'))['model'].state_dict())
+        else:
+            models[m] = torch.hub.load('/home/ubuntu/Downloads/test-yolo-detect/yolov5', m, force_reload=True, skip_validation=True)
+
+        # models[m] = torch.hub.load('ultralytics/yolov5', m, force_reload=True, skip_validation=True)
+
+
+
+    app.run(host='0.0.0.0', port=80, debug=True)  # debug=True causes Restarting with stat
 
